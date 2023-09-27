1d7c542a1590ee9e5762bf46e26dcdae321487e0
Pari Gupta <143111684+PARIGUPTA0011@users.noreply.github.com>
2023-09-27 19:42:17 +0530

    Create Q2.cpp

---
+++ Q2.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+using namespace std ;
+int marks;
+cin>>marks;
+    
+
+if(marks>90){
+    cout<<"Excellent";
+
+}
+if(80<marks<=90){
+    cout<<"Good";
+}
+if(70<marks<=80){
+    cout<<"Fair";
+
+}
+if(60<marks<=70){
+    cout<<"meets expectations";
+
+}else{
+    cout<<"below par ";
+}
+    return 0 ;
+}
