738a92b6992439f7e81c98757ed1eb26dfaf043c
Pari Gupta <143111684+PARIGUPTA0011@users.noreply.github.com>
2023-09-28 18:50:07 +0530

    Update Q1.cpp

--- Q1.cpp
+++ Q1.cpp
@@ -4,6 +4,19 @@ using namespace std ;
 int main() {
 
 
+  int i,j;
+  for(i=1;i<=5;i++){
+    for(j=1;j<=5;j++)
+     if (i==1||i==5||j==6-i)
+     {
+        cout<<"*";
+     }
+  else{
+    cout<<" ";
+  }
+   cout<<endl;
+    }
+
 
 
 
